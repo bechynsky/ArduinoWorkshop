bd019be65dae56fc9644b338f52275f67f0c1c11
Štěpán Bechynský <stepan@bechynsky.cz>
2015-07-10 13:49:39 +0200

    Simple alarm

---
+++ Alarm/Alarm.ino
@@ -0,0 +1,119 @@
+#include <Wire.h>
+#include <Time.h>
+#include <DS1307RTC.h>
+#include <TM1636.h>
+
+// tlacitka
+#define KEY_DOWN  9
+#define KEY_UP	  10
+#define KEY_MENU  11
+
+#define ALARM_BUZZER 6
+#define ALARM_DURATION 500
+#define ALARM_TONE 1000
+
+TM1636 tm1636(7,8);
+
+// struktura pro cas
+tmElements_t tm;
+// obsah pro displej
+int8_t t[] = {0, 0, 0, 0};
+// pozice pro nastaveni cisla
+int menuPosition = -1; 
+// hodiny, minuty
+int h, m = 0;
+
+void setup() {
+  Serial.begin(9600);
+  tm1636.init();
+  pinMode(KEY_DOWN, INPUT_PULLUP);
+  pinMode(KEY_UP, INPUT_PULLUP);
+  pinMode(KEY_MENU, INPUT_PULLUP);
+  pinMode(ALARM_BUZZER, OUTPUT);
+  
+  alarm();
+}
+
+void loop() {
+  if(digitalRead(KEY_MENU) == LOW)
+  {
+    delay(20);
+    if(digitalRead(KEY_MENU) == LOW)
+    {
+      menuPosition++;
+      if (menuPosition > 3) {
+        menuPosition = -1;
+      }
+    }
+  }
+
+  if (menuPosition > -1) {
+    if(digitalRead(KEY_UP) == LOW)
+    {
+      delay(30);
+      if(digitalRead(KEY_UP) == LOW)
+      {
+        t[menuPosition]++;
+        if (t[menuPosition] > 9) {
+          t[menuPosition] = 0;
+        }
+      }
+    }
+    
+    if(digitalRead(KEY_DOWN) == LOW)
+    {
+      delay(30);
+      if(digitalRead(KEY_DOWN) == LOW)
+      {
+        t[menuPosition]--;
+        if (t[menuPosition] < 0) {
+          t[menuPosition] = 9;
+        }
+      }
+    }
+    
+    validateAlarmTime();
+  }
+  
+  if (RTC.read(tm)) {
+    h = tm.Hour;
+    m = tm.Minute;
+    
+    if (h == ((t[0] * 10) + t[1]) && (m == ((t[2] * 10) + t[3]))) {
+      alarm();
+    }
+  }
+  
+  tm1636.point(POINT_ON);
+  tm1636.display(t);
+  delay(100);
+}
+
+void validateAlarmTime() {
+  if (t[0] > 2) {
+    t[0] = 2;
+  }
+  
+  if (((t[0] * 10) + t[1]) > 23) {
+    t[0] = 2;
+    t[1] = 3;
+  }
+
+  if (t[2] > 5) {
+    t[2] = 5;
+  }
+  
+  if (((t[2] * 10) + t[3]) > 59) {
+    t[2] = 5;
+    t[3] = 9;
+  }
+}
+
+void alarm() {
+  for (long i = 0; i < ALARM_DURATION * 1000L; i += ALARM_TONE * 2) {
+    digitalWrite(ALARM_BUZZER, HIGH);
+    delayMicroseconds(ALARM_TONE);
+    digitalWrite(ALARM_BUZZER, LOW);
+    delayMicroseconds(ALARM_TONE);
+  }
+}
