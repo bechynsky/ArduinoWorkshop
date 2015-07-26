ddbd0bba11728a56f0300397c5a2ec554295b5aa
Štěpán Bechynský <stepan@bechynsky.cz>
2015-07-26 16:53:26 +0200

    Fixed delay value

--- BuzzSimple/BuzzSimple.ino
+++ BuzzSimple/BuzzSimple.ino
@@ -1,14 +1,14 @@
 #define BUZZER 6
-#define PAUSE 2
+#define PAUSE 1000
 
 void setup() {
   pinMode(BUZZER, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(BUZZER, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(BUZZER, HIGH);   // turn the BUZZER on (HIGH is the voltage level)
   delay(PAUSE);              // wait for a second
-  digitalWrite(BUZZER, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(BUZZER, LOW);    // turn the BUZZER off by making the voltage LOW
   delay(PAUSE);              // wait for a second
 }
 
