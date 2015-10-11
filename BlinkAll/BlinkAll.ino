65f92a431096fc9999f3ac7dcd94fbeda951e683
Štěpán Bechynský <stepan@bechynsky.cz>
2015-10-11 08:54:19 +0200

    Bit mask as variable

--- BlinkAll/BlinkAll.ino
+++ BlinkAll/BlinkAll.ino
@@ -1,13 +1,16 @@
 // 1 sekunda
 #define PAUSE 1000
+#define MASK 0b00111100
 
 void setup() {
   // porty 2,3,4,5 jako vystup
-  DDRD |= 0b00111100;
+  // DDRD = DDRD | 0b00111100;
+  DDRD |= MASK; //0b00111100;
+  PORTD = MASK; //0b00111100;
 }
 
 void loop() {
   // preklopeni hodnot
-  PORTD ^= 0b00111100;
+  PORTD ^= MASK; //0b00111100;
   delay(PAUSE);
 }
