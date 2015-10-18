be0832ae1be13d0d2742684367259065323414f4
Štěpán Bechynský <stepan@bechynsky.cz>
2015-10-18 11:51:44 +0200

    Update map and bit mask

--- LightScale/LightScale.ino
+++ LightScale/LightScale.ino
@@ -1,6 +1,7 @@
 const byte SCALE_VALUE[] = { 0b00000100, 0b00001100, 0b00011100, 0b00111100};
 
-int senzor = A0;
+// fotorezistor - A1, termistor - A0
+int senzor = A1;
 
 void setup() {
   Serial.begin(9600);
@@ -13,10 +14,10 @@ void loop() {
   int light = analogRead(senzor);
   Serial.println(light);
   
-  int scale = map(light, 400, 800, 0, 3);
+  int scale = map(light, 300, 1000, 0, 3);
   Serial.println(scale);
   
-  PORTD = 0b00000000;
+  PORTD &= !0b00111100; // nastaveni vychoziho stavu - zhasnuto
   PORTD |= (0b00000100 << scale);
   // PORTD |= SCALE_VALUE[scale];
   
